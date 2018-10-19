5595d22f27ec50422a66bbb484d74bb00f1a79fa
Mr.yan <40586009+Mryan2005@users.noreply.github.com>
2018-10-19 21:49:11 +0800

    add info

--- src/main/main.java
+++ src/main/main.java
@@ -3,5 +3,16 @@ package main;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class main extends JavaPlugin {
-
+	@Override
+	public void onEnable() {
+		getLogger().info("插件启动");
+	}
+	@Override
+	public void onDisable() {
+		System.out.println("插件关闭");
+	}
+	@Override
+	public void onLoad() {
+		System.out.println("插件重载");
+	}
 }
