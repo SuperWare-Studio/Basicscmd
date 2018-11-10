697c6e97f97d4e8532ca070a9c5ef3c5239632d3
Mr.yan <40586009+Mryan2005@users.noreply.github.com>
2018-11-11 07:39:33 +0800

    add VUpdateAPI

--- src/main/main.java
+++ src/main/main.java
@@ -1,5 +1,6 @@
 package main;
 
+import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
@@ -8,38 +9,24 @@ import org.bukkit.plugin.java.JavaPlugin;
 public class main extends JavaPlugin {
 	@Override
 	public void onEnable() {
-		getLogger().info("插件启动完成");
+		System.out.println("插件被启动了");
 	}
 	@Override
 	public void onDisable() {
-		System.out.println("插件卸载完成");
+		System.out.println("插件被关闭了");
 	}
 	@Override
 	public void onLoad() {
-		System.out.println("插件重载完成");
-	}	
-	//command
-	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-		//about kill someone
-		if(command.getName().equalsIgnoreCase("kill myself")) {
-			if(sender instanceof Player) {
-				Player p = (Player) sender;
-				//强制转换sender为玩家
-				p.setHealth(0);
-				//设置生命值为0，杀死玩家
-				p.sendMessage("§c您自杀了");
-				return true;
-				}
-			}
-				if(args.length == 0) {
-					sender.sendMessage("§c/kill myself");
-					return true;
-				}
-				if(!sender.hasPermission("Basicscmd.killmyself")) {
-					sender.sendMessage("§c您没有这个命令的权限");
-					return true;
-				}
-
-		return false;
+		System.out.println("插件已重载");
 	}
+	//command
+	@Override
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+        if(label.equalsIgnoreCase("hello")){
+             String playerName = args[0];   //获取这个名字
+			 Player player = Bukkit.getPlayer(playerName);  //让Spigot在服务器找一个叫playerName的玩家，如果不存在，就是null
+			 player.sendMessage(sender.getName()+"对你说：你好！");   //发送信息
+        }
+    return true;
+    }
 }
